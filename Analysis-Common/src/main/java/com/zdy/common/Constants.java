f4b2cfe91816d1b2f3962faa616fce2d4e420e6e
Home <1525714537@qq.com>
2018-12-16 14:29:00 +0800

    ignore

--- Analysis-Common/src/main/java/com/zdy/common/Constants.java
+++ Analysis-Common/src/main/java/com/zdy/common/Constants.java
@@ -1,13 +1,17 @@
 package com.zdy.common;
 
 /**
- * 常量类
+ * 公共常量
  */
 public class Constants {
-	//主题
+	//kafka的topic
 	public static final String TOPIC_APP_STARTUP = "topic-app-startup" ;
 	public static final String TOPIC_APP_ERRROR = "topic-app-error" ;
 	public static final String TOPIC_APP_EVENT = "topic-app-event" ;
 	public static final String TOPIC_APP_USAGE = "topic-app-usage" ;
 	public static final String TOPIC_APP_PAGE = "topic-app-page" ;
+	
+	public static String TIMESTAMP = "timestamp";
+	public static String PRESERVE = "preserveExisting";
+	public static boolean PRESERVE_DFLT = false;
 }
